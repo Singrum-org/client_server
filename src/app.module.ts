4b68c598d8666127c205d15b5774cda6faf93e91
OH-JOO-YEONG <brb1111@naver.com>
2023-06-28 22:08:15 +0900

    [Fix] OrmModule 안되서 module에 직접 넣는 형식으로 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PlantsModule } from './plants/plants.module';
-import { typeORMConfig } from './configs/typeorm.config';
 
 @Module({
   imports: [
@@ -10,7 +9,16 @@ import { typeORMConfig } from './configs/typeorm.config';
       cache: true,
       isGlobal: true,
     }),
-    TypeOrmModule.forRoot(typeORMConfig),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [__dirname + './**/*.entity.{js,ts}'],
+      synchronize: true,
+    }),
     PlantsModule,
   ],
 })
--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,10 @@ import { setupSwagger } from 'src/utils/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api'); // <- global prefix
 
   const port = Number(process.env.PORT);
+
   setupSwagger(app);
 
   await app.listen(port);
